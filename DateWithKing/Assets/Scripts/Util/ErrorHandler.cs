38487beaca5b606e84f24c5dfd7ccac77ca1dfdb
강승연 <syeon.dev@gmail.com>
2024-12-31 21:24:28 +0900

    [XML] PrintError 주석 추가

--- DateWithKing/Assets/Scripts/Util/ErrorHandler.cs
+++ DateWithKing/Assets/Scripts/Util/ErrorHandler.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 /// </summary>
 public static class ErrorHandler
 {
+    /// <summary>
+    /// 에러 로그를 출력.
+    /// </summary>
+    /// <param name="error"> enum Error에서 정의한 에러 </param>
     public static void PrintError(Define.Error error)
     {
         Debug.LogError(error.ToString());
