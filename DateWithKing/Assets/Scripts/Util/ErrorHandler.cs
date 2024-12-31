4f82b8fd97a682c5a0239e1b87184e2f3c0faf29
강승연 <syeon.dev@gmail.com>
2024-12-31 21:24:09 +0900

    [XML] ErrorHandler 클래스 주석 추가

--- DateWithKing/Assets/Scripts/Util/ErrorHandler.cs
+++ DateWithKing/Assets/Scripts/Util/ErrorHandler.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 에러/오류를 출력.
+/// </summary>
 public static class ErrorHandler
 {
     public static void PrintError(Define.Error error)
