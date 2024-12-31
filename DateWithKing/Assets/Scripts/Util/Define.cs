75595f7583dd675f57d855b1177eb15bfba51420
강승연 <syeon.dev@gmail.com>
2024-12-31 16:40:30 +0900

    [INIT] Extensions, Define 스크립트 추가

---
+++ DateWithKing/Assets/Scripts/Util/Define.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// enum 정의를 모음.
+/// 해당 클래스 외에 public enum을 만들지 않음.
+/// </summary>
+public class Define
+{
+    
+}
---
+++ DateWithKing/Assets/Scripts/Util/Extensions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+///<summary>
+/// c# extension 함수를 모음.
+/// 해당 클래스 외에 extension 함수를 만들지 않음. 
+///</summary>
+public static class Extensions
+{
+    
+}
