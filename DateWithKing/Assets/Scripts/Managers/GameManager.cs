5dc73487bc3d12af7493b503680c0ede4c3279b4
강승연 <syeon.dev@gmail.com>
2025-01-02 16:02:08 +0900

    [FEAT] 게임 매니저 임시 게임 데이터 세팅 기능 추가

--- DateWithKing/Assets/Scripts/Managers/GameManager.cs
+++ DateWithKing/Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameData data { get; set; }
+
+    /// <summary>
+    /// 최초 게임 데이터 세팅 <br/>
+    /// </summary>
+    public void InitData(CustomizingDTO customizingDto)
     {
-        
+        data = DataLoader.ReadData<GameData>();
+        data.SetCustomizingData(customizingDto);
+    }
+    
+    /// <summary>
+    /// 세이브 데이터 로드 <br/>
+    /// </summary>
+    /// <param name="slot">세이브 데이터 슬롯 번호</param>
+    public void LoadData(int slot)
+    {
+        GameData load = DataLoader.ReadData<GameData>((Define.DynamicData)slot);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 세이브 데이터 저장
+    /// </summary>
+    /// <param name="slot"> 세이브 데이터 슬롯 번호</param>
+    public void SaveData(int slot)
     {
-        
+        DataLoader.WriteData((Define.DynamicData)slot, data);
     }
 }
