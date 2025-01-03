bebeb51d6fd2916bcc821f7ba97ac4b69caafd38
강승연 <syeon.dev@gmail.com>
2025-01-03 13:01:16 +0900

    [UPDATE] BackgroundScreenPresenter에 Cycle 기능을 삭제

--- DateWithKing/Assets/Scripts/Screen/Base/BackgroundScreenPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/Base/BackgroundScreenPresenter.cs
@@ -2,10 +2,5 @@ using UnityEngine;
 
 public class BackgroundScreenPresenter : MonoBehaviour
 {
-    private static ScreenCycle cycle;
 
-    void Awake()
-    {
-        cycle = GetComponent<ScreenCycle>();
-    }
 }
