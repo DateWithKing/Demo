81ffcc017a44c85fa17a34f362dc55ec8523f0fe
강승연 <syeon.dev@gmail.com>
2025-01-04 15:13:48 +0900

    [STYLE] Game 씬/스크린 이름 Semester로 변경
    - 씬 이동 시 하드코딩 해뒀던 것도 변경

--- DateWithKing/Assets/Scripts/Screen/CustomizingScreen/CustomizingPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/CustomizingScreen/CustomizingPresenter.cs
@@ -21,6 +21,6 @@ public class CustomizingPresenter : Presenter
     private void GameStart()
     {
         GameManager.Instance.InitData(view.GetCustomizingData());
-        screen.MoveScene("Game");
+        screen.MoveScene("Semester");
     }
 }
