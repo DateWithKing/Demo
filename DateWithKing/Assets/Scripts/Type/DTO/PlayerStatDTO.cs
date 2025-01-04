58b65ec9f5cf235320bef729f15ca7792dcf9661
강승연 <syeon.dev@gmail.com>
2025-01-05 02:53:04 +0900

    [ADD] CustomizingDTO 제외 게임 상태 저장 목적의 DTO 생성(1차 기획서 기준)

---
+++ DateWithKing/Assets/Scripts/Type/DTO/PlayerStatDTO.cs
@@ -0,0 +1,16 @@
+
+    using UnityEditor;
+
+    public class PlayerStatDTO
+    {
+        public int bonusHp = 0; //다음날 Hp 변동 수치(ex. 10이면 다음날 원래 hp + 10된 값으로 시작
+        public int hp = 5;
+        public int str = 1;
+        public int wis = 1;
+        public int otk = 1;
+        public int slave = 1;
+        public int lvSan = 0;
+        public int lvHyun = 0;
+        public int lvPyo = 0;
+        public int gold = 150;
+    }
---
+++ DateWithKing/Assets/Scripts/Type/DTO/SettingDTO.cs
@@ -0,0 +1,6 @@
+
+    public class SettingDTO
+    {
+        public int bgmVolume = 3;
+        public int effectVolume = 3;
+    }
