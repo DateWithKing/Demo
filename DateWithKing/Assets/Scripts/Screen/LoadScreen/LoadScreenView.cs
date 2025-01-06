5bc3c765fee88307b6de646605c17b8734e63347
강승연 <syeon.dev@gmail.com>
2025-01-07 00:50:41 +0900

    Revert "[FEAT] LoadScreenView 구현"
    This reverts commit fd16b746d47676cb00c0cfb6f57d9a4fb1784c1e.

--- DateWithKing/Assets/Scripts/Screen/LoadScreen/LoadScreenView.cs
+++ DateWithKing/Assets/Scripts/Screen/LoadScreen/LoadScreenView.cs
@@ -1,37 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class LoadScreenView : View, ILoadScreenView
+public class LoadScreenView : MonoBehaviour
 {
-
-    [SerializeField] private Button slot;
-    
-    Slot slotInstance = new Slot();
-
-
-
-    public event Action<int> ClickSlot;
-    
-
+    // Start is called before the first frame update
     void Start()
     {
-        int slotID = slotInstance.slotID;
-        slot.onClick.AddListener(() => { ClickSlot?.Invoke(slotID); });
+        
     }
 
-
-    /// <summary>
-    /// 화면에 슬롯 데이터 출력
-    /// </summary>
-    /// <param name="slot"> 출력할 슬롯 번호 (0, 1, 2 순) </param>
-    /// <param name="slotDTO"> 출력할 슬롯 데이터 </param>
-    public void PrintSlot(int slot, SlotDTO slotDTO)
+    // Update is called once per frame
+    void Update()
     {
-        slotInstance.PrintSlot(slot, slotDTO);
-
+        
     }
 }
