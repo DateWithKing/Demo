0f605720bf6ab685f68f1799ecb6e2b8480a64c8
강승연 <syeon.dev@gmail.com>
2025-01-08 01:29:15 +0900

    [FEAT] 주차 업데이트 기능 추가

--- DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/DayScreen/BackgroundPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/DayScreen/BackgroundPresenter.cs
@@ -19,11 +19,16 @@ public class BackgroundPresenter : Presenter
 
     private void UpdateBackgroundScreen()
     {
+        //Intro로 돌아가면 Date 업데이트
+        if (weekCycle == WeekCycle.Intro 
+            && SemesterSceneData.Instance.clock.GetCurrentWeekCycle() == WeekCycle.Intro)
+        {
+            GameManager.Instance.data.date.NextDate(); 
+        }
         //현재 씬 활성화
         if (SemesterSceneData.Instance.clock.GetCurrentWeekCycle() == weekCycle)
         {
             screen.ShowScreen();
-            Debug.Log(gameObject);
         }
         //직전 weekCycle 씬 제외 전부 비활성화
         else if ((int)SemesterSceneData.Instance.clock.GetCurrentWeekCycle() - 1 != (int)weekCycle)
