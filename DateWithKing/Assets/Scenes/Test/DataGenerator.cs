e56e98b4702660cc5ad8b43c917adeac4a168184
강승연 <syeon.dev@gmail.com>
2025-01-08 17:15:33 +0900

    [FEAT] 데이터 생성용 기능성 씬 추가

---
+++ DateWithKing/Assets/Scenes/Test/DataGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataGenerator : MonoBehaviour
+{
+    void Start()
+    {
+        //예시 작성. 절대 사용하지 말 것. (사용하면 데이터 덮어쓰기 됨)
+        //NightActivityData nightActivityData = new NightActivityData();
+        //DataLoader.WriteData(nightActivityData); 
+    }
+}
