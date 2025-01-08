735c46a4dea2e26d6a9ff7739d1ced76e48c9cbb
강승연 <syeon.dev@gmail.com>
2025-01-08 17:13:52 +0900

    [FEAT] 밤 활동 정보 출력(로그)
    - 로직은 추후 짤 것

--- DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/NightActivityScreen/NightActivityPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/NightActivityScreen/NightActivityPresenter.cs
@@ -2,7 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NightActivityPresenter : Screen
+public class NightActivityPresenter : Presenter
 {
+    private Screen _screen;
 
+    private Screen screen
+    {
+        get
+        {
+            if (_screen == null) _screen = GetComponent<Screen>();
+            return _screen;
+        }
+        set => _screen = value;
+    }
+
+    void Awake()
+    {
+        screen = GetComponent<Screen>();
+    }
+    public void DoActivity(string activity)
+    {
+        screen.ShowScreen();
+        Debug.Log($"{activity}을/를 했다!");
+        Debug.Log($"{SemesterSceneData.Instance.nightActivity.deltaStat[activity].hpCost}만큼 체력을 사용했다. ");
+    }
 }
