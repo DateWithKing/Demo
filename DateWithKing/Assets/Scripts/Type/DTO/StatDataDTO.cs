33b793a384bfd67deca438c55b57e78fe5ded2fd
강승연 <syeon.dev@gmail.com>
2025-01-16 20:15:21 +0900

    [UPDATE] 모든 스탯에 Stat 래퍼 클래스 적용

--- DateWithKing/Assets/Scripts/Type/DTO/StatDataDTO.cs
+++ DateWithKing/Assets/Scripts/Type/DTO/StatDataDTO.cs
@@ -1,33 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StatDataDTO
 {
-    public int hp = 5; // 초기 값 설정
-    public int str = 1;
-    public int intel = 1;
-    public int slave = 1;
-    
-    public int otk = 1;
-    public int bonusHp = 0; //다음날 Hp 변동 수치(ex. 10이면 다음날 원래 hp + 10된 값으로 시작
-    public int lvSan = 0;
-    public int lvHyun = 0;
-    public int lvPyo = 0;
-    public int gold = 150;
+    public Stat hp = new Stat(5, 5, 10); // 초기 값 설정
+    public Stat str = new Stat(1, 1, 10);
+    public Stat intel = new Stat(1, 1, 10);
+    public Stat slave = new Stat(1, 1, 10);
+    public Stat otk = new Stat(1, 1, 10);
+    public Stat bonusHp = new Stat(0, 0, 50); //다음날 Hp 변동 수치(ex. 10이면 다음날 원래 hp + 10된 값으로 시작
+    public Stat lvSan = new Stat(0, -50, 100);
+    public Stat lvHyun = new Stat(0, -50, 100);
+    public Stat lvPyo = new Stat(0, -50, 100);
+    public Stat gold = new Stat(150, 0, Int32.MaxValue);
 
     public StatDataDTO()
     {
 
-    }
-
-    // 값을 받아와서 설정하는 생성자 메서드
-    public StatDataDTO(int hp, int str, int intel, int slave, int otk)
-    {
-        this.hp = hp;
-        this.str = str;
-        this.intel = intel;
-        this.slave = slave;
-        this.otk = otk;
     }
 }
