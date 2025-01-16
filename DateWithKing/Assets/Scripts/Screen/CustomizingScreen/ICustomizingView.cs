e27a43a488f2ae579f51086d17bf9c2d15dab2cd
강승연 <syeon.dev@gmail.com>
2025-01-16 17:26:44 +0900

    [WIP] Customizing 씬 임시 봉쇄

--- DateWithKing/Assets/Scripts/Screen/CustomizingScreen/ICustomizingView.cs
+++ DateWithKing/Assets/Scripts/Screen/CustomizingScreen/ICustomizingView.cs
@@ -6,6 +6,6 @@ using UnityEngine.Events;
 public interface ICustomizingView
 {
     public event UnityAction StartGame;
-    public void SetCustomizingData(CustomizingDTO customizingDto);
-    public CustomizingDTO GetCustomizingData();
+    /*public void SetCustomizingData(CustomizingDTO customizingDto);
+    public CustomizingDTO GetCustomizingData();*/
 }
