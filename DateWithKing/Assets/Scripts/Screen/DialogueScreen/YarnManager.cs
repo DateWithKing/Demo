10bf0fc0e28ad976ecf03f307581eb7b7d3022d5
Ssomewhat <isc10120@gmail.com>
2025-01-18 21:32:42 +0900

    [FEAT] 다이얼로그 시스템 기능 추가

--- DateWithKing/Assets/Scripts/Screen/DialogueScreen/YarnManager.cs
+++ DateWithKing/Assets/Scripts/Screen/DialogueScreen/YarnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using Yarn.Unity;
 
+
 public class YarnManager : SceneSingleton<YarnManager>
 {
     DialogueRunner runner;
@@ -13,6 +14,9 @@ public class YarnManager : SceneSingleton<YarnManager>
 
     [SerializeField]
     private Image CharacterImage;
+
+    [SerializeField]
+    private Image BackgroundImage;
 
     [SerializeField]
     private AudioSource SoundEffectAS;
@@ -32,9 +36,15 @@ public class YarnManager : SceneSingleton<YarnManager>
         runner = GameObject.FindAnyObjectByType<DialogueRunner>();
         runner.AddCommandHandler("end", EndDialogue);
         runner.AddCommandHandler<string>("notice", Notice);
-        runner.AddCommandHandler<string>("show", ShowImage);
+        runner.AddCommandHandler<string>("dislike", (name)=>Notice(name+"(이/가) 싫어합니다."));
+        runner.AddCommandHandler<string>("like", (name)=>Notice(name+"(이/가) 좋아합니다."));
+        runner.AddCommandHandler<string>("show", ShowCharactor);
+        runner.AddCommandHandler<string>("bg", ShowBackground);
         runner.AddCommandHandler<string>("play", SoundEffect);
         runner.AddCommandHandler<string, string>("choice", StartChoice);
+        runner.AddCommandHandler<string, int>("change", SetStat);
+        runner.AddCommandHandler<int>("recover_hp", RecoverHp);
+        runner.AddCommandHandler<int>("use_hp", UseHp);
     }
 
     /// <summary>
@@ -54,12 +64,14 @@ public class YarnManager : SceneSingleton<YarnManager>
     /// 얀 스크립트에서 대화 종료시 호출해야함. <br/>
     /// 기능: <br/>
     /// 캐릭터 이미지 비활성화 <br/>
+    /// 배경경 이미지 비활성화 <br/>
     /// 다이얼로그 씬 비활성화 <br/>
     /// </summary>
     void EndDialogue()
     {
         dialogueScreen.HideScreen();
         CharacterImage.gameObject.SetActive(false);
+        BackgroundImage.gameObject.SetActive(false);
     }
 
     /// <summary>
@@ -72,12 +84,21 @@ public class YarnManager : SceneSingleton<YarnManager>
     }
 
     /// <summary>
-    /// <see cref="spriteName"/>에 해당하는 이미지를 보이게 함 (캐릭터 스프라이트 띄울 때 사용)
+    /// <see cref="spriteName"/>에 해당하는 캐릭터 이미지를 보이게 함 (변경 포함)
     /// </summary>
     /// <param name="spriteName">Resorces/Sprites 폴더에 있는 스프라이트여야 함</param>
-    void ShowImage(string spriteName){
-        CharacterImage.sprite = Resources.Load<Sprite>("Sprites/"+spriteName);
+    void ShowCharactor(string spriteName){
+        CharacterImage.sprite = Resources.Load<Sprite>("Sprites/Charactor/"+spriteName);
         CharacterImage.SetNativeSize();
+        CharacterImage.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// <see cref="spriteName"/>에 해당하는 배경 이미지를 보이게 함 (변경 포함)
+    /// </summary>
+    /// <param name="spriteName"></param>
+    void ShowBackground(string spriteName){
+        BackgroundImage.sprite = Resources.Load<Sprite>("Sprites/Background/"+spriteName);
         CharacterImage.gameObject.SetActive(true);
     }
 
@@ -94,19 +115,86 @@ public class YarnManager : SceneSingleton<YarnManager>
     /// <param name="negNode"></param>
     void StartChoice(string posNode, string negNode){
 
+        BackgroundController.Instance.ChangeImage(Background.Looking);
+
         // openCV 도입하면 바꿀부분
         PosNegPanel.SetActive(true);
 
         PosNegPanel.transform.GetChild(0).GetComponent<Button>().onClick.RemoveAllListeners();
         PosNegPanel.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(()=>{
             PosNegPanel.SetActive(false);
-            RunDialogue(posNode);});
+            RunDialogue(posNode);
+            BackgroundController.Instance.ChangeImage(Background.Day);});
 
         PosNegPanel.transform.GetChild(1).GetComponent<Button>().onClick.RemoveAllListeners();
         PosNegPanel.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(()=>{
             PosNegPanel.SetActive(false);
-            RunDialogue(negNode);});
+            RunDialogue(negNode);
+            BackgroundController.Instance.ChangeImage(Background.Day);});
 
+    }
+
+    /// <summary>
+    /// 유저 이름 반환
+    /// </summary>
+    /// <param name="text"></param>
+    [YarnFunction("name")]
+    public static string GetName(){
+        return GameManager.Instance.data.name;
+    }
+
+    /// <summary>
+    /// <see cref="statName"/>에 해당하는 스탯 수치 반환
+    /// </summary>
+    /// <param name="statName"></param>
+    [YarnFunction("get")]
+    public static int GetStat(string statName){
+        return GameManager.Instance.data.stats[statName].value;
+    }
+
+    /// <summary>
+    /// 최댓값 100기준의 현재 hp 반환
+    /// </summary>
+    [YarnFunction("get_hp")]
+    public static int GetHp(){
+        return SemesterSceneData.Instance.hp.GetHp();
+    }
+
+    /// <summary>
+    /// val만큼 현재 hp를 감소시킴
+    /// </summary>
+    /// <param name="val"></param>
+    void UseHp(int val){
+        if(SemesterSceneData.Instance.hp is not null){
+            SemesterSceneData.Instance.hp.UseHp(val);
+        }
+        else{
+            Debug.LogError("SemesterSceneData.Instance.hp가 존재하지 않음");
+        }
+    }
+
+    /// <summary>
+    /// val만큼 현재 hp를 증가시킴킴
+    /// </summary>
+    /// <param name="val"></param>
+    void RecoverHp(int val){
+        if(SemesterSceneData.Instance.hp is not null){
+            SemesterSceneData.Instance.hp.RecoverHp(val);
+        }
+        else{
+            Debug.LogError("SemesterSceneData.Instance.hp가 존재하지 않음");
+        }
+    }
+
+
+    /// <summary>
+    /// <see cref="statName"/>에 해당하는 스탯 수치 <see cref="val"/>만큼 조정
+    /// </summary>
+    /// <param name="statName"></param>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    void SetStat(string statName, int val){
+        GameManager.Instance.data.stats[statName].ChangeStat(val);
     }
 
 }
