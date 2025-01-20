0b2fe268b08661fa453bd799681e744d565ceb04
강승연 <syeon.dev@gmail.com>
2025-01-21 01:42:20 +0900

    [UPDATE] 10일 후 종강총회로 변경

--- DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/SemesterScreenPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/SemesterScreenPresenter.cs
@@ -7,7 +7,7 @@ public class SemesterScreenPresenter : MonoBehaviour
     /// <summary>
     /// 한 학기 일수
     /// </summary>
-    [SerializeField] private int semesterDays = 14;
+    [SerializeField] private int semesterDays = 10;
 
     private Screen screen;
 
