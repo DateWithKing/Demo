bdf168d0773220d438d0f98cdf8c7f7fe0324af3
강승연 <syeon.dev@gmail.com>
2025-01-21 15:36:41 +0900

    [FEAT] 바탕 변경 기능 구현

--- DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/DayScreen/BackgroundPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/GameScreen/BackgroundScreen/DayScreen/BackgroundPresenter.cs
@@ -19,6 +19,11 @@ public class BackgroundPresenter : Presenter
         SemesterSceneData.Instance.clock.TimeChanged += UpdateBackgroundScreen;
     }
 
+    void OnEnable()
+    {
+        BackgroundController.Instance.ChangeImage(Enum.Parse<Background>(weekCycle.ToString()));
+    }
+
     private void UpdateBackgroundScreen()
     {
         //Intro로 돌아가면 Date 업데이트
