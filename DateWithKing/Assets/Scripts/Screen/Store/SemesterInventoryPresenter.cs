ca228204747c8a451c353f0cd8e7d9500c030e79
강승연 <syeon.dev@gmail.com>
2025-01-29 00:54:46 +0900

    [FEAT] 인벤토리 아이템 사용 구현

--- DateWithKing/Assets/Scripts/Screen/Store/SemesterInventoryPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/Store/SemesterInventoryPresenter.cs
@@ -2,6 +2,12 @@ public class SemesterInventoryPresenter : InventoryPresenter
 {
     protected override void UseItem(int slot)
     {
-        throw new System.NotImplementedException();
+        Item item = GameManager.Instance.data.inventory.GetItem(slot);
+        if (item.type != ItemType.체력)
+        {
+            Popup.Instance.PopPanel("사용할 수 없습니다. ");
+            return;
+        } 
+        GameManager.Instance.data.inventory.UseItem(slot);
     }
 }
