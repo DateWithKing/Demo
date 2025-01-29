e3b7de88995fa93eda069abe25396607259a62c3
강승연 <syeon.dev@gmail.com>
2025-01-29 23:57:26 +0900

    [FEAT] 밤에 캐릭터 만나는 이벤트 추가

--- DateWithKing/Assets/Scripts/Screen/Semester/NightActivityScreen/NightActivityPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/Semester/NightActivityScreen/NightActivityPresenter.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NightActivityPresenter : Presenter
 {
     private Screen _screen;
+    public const string SpotImagePath = "Sprites/Spot/";
+    [SerializeField] private Image spotImage;
 
     private Screen screen
     {
@@ -22,8 +25,25 @@ public class NightActivityPresenter : Presenter
     }
     public void DoActivity(string activity)
     {
+        spotImage.sprite = Resources.Load<Sprite>(SpotImagePath + "밤");
         screen.ShowScreen();
         string dialogue = $"밤_{activity}";
-        YarnManager.Instance.RunDialogue(dialogue);
+        YarnManager.Instance.RunDialogue(dialogue, delegate { MeetGirls(activity);});
+    }
+    
+    private void MeetGirls(string activity)
+    {
+        string spotData = $"밤_{GameManager.Instance.data.date.GetCurrentWeek()}_{activity}";
+        
+        if (SemesterSceneData.Instance.DayDialogue.spotCharacters.ContainsKey(spotData))
+        {
+            spotImage.sprite = Resources.Load<Sprite>(SpotImagePath + activity);
+            Debug.Log($"{spotData}에 방문해 {SemesterSceneData.Instance.DayDialogue.spotCharacters[spotData]}을/를 만났습니다.");
+            YarnManager.Instance.RunDialogue(spotData);
+        }
+        else
+        {
+            Debug.Log("아무도 없습니다.");
+        }
     }
 }
