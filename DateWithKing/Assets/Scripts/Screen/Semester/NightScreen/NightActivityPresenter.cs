1cdf2edb8e9d6bc72eef4d915f50524cf2e2bdad
강승연 <syeon.dev@gmail.com>
2025-02-02 09:03:33 +0900

    [FEAT] 밤 캐릭터 만남 다이얼로그 출력

--- DateWithKing/Assets/Scripts/Screen/Semester/NightScreen/NightActivityPresenter.cs
+++ DateWithKing/Assets/Scripts/Screen/Semester/NightScreen/NightActivityPresenter.cs
@@ -7,7 +7,8 @@ public class NightActivityPresenter : Presenter
 {
     private Screen _screen;
     public const string SpotImagePath = "Sprites/Spot/";
-    [SerializeField] private Image spotImage;
+    [SerializeField] private Image meetGirls;
+    private string currentActivity;
 
     private Screen screen
     {
@@ -23,27 +24,34 @@ public class NightActivityPresenter : Presenter
     {
         screen = GetComponent<Screen>();
     }
+
     public void DoActivity(string activity)
     {
-        spotImage.sprite = Resources.Load<Sprite>(SpotImagePath + "밤");
+        currentActivity = activity;
         screen.ShowScreen();
-        string dialogue = $"밤_{activity}";
-        YarnManager.Instance.RunDialogue(dialogue, delegate { MeetGirls(activity);});
+        MeetGirls();
+    }
+    public void PrintActivityDialogue()
+    {
+        string dialogue = $"밤_{currentActivity}";
+        YarnManager.Instance.RunDialogue(dialogue);
     }
     
-    private void MeetGirls(string activity)
+    public void MeetGirls()
     {
-        string spotData = $"밤_{GameManager.Instance.data.date.GetCurrentWeek()}_{activity}";
-        
+        string spotData = $"밤_{GameManager.Instance.data.date.GetCurrentWeek()}_{currentActivity}";
         if (SemesterSceneData.Instance.DayDialogue.spotCharacters.ContainsKey(spotData))
         {
-            spotImage.sprite = Resources.Load<Sprite>(SpotImagePath + activity);
+            meetGirls.gameObject.SetActive(true);
+            meetGirls.sprite = Resources.Load<Sprite>(SpotImagePath + currentActivity);
             Debug.Log($"{spotData}에 방문해 {SemesterSceneData.Instance.DayDialogue.spotCharacters[spotData]}을/를 만났습니다.");
             YarnManager.Instance.RunDialogue(spotData);
         }
         else
         {
+            meetGirls.gameObject.SetActive(false);
             Debug.Log("아무도 없습니다.");
+            PrintActivityDialogue();
         }
     }
 }
