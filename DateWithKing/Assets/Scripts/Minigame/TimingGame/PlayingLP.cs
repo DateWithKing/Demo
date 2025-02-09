96afaea61fb5851796b0a7db3097cf52cf8b7d6d
강승연 <syeon.dev@gmail.com>
2025-02-09 22:24:13 +0900

    [BUILD] 3차 빌드

--- DateWithKing/Assets/Scripts/Minigame/TimingGame/PlayingLP.cs
+++ DateWithKing/Assets/Scripts/Minigame/TimingGame/PlayingLP.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using UnityEditor.U2D.Sprites;
 using UnityEngine;
 using Yarn.Unity;
 
