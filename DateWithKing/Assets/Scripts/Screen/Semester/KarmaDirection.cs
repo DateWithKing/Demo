70e87f9f456c445ed6e5bc126e720bc16f4dcf3a
강승연 <syeon.dev@gmail.com>
2025-02-09 16:32:40 +0900

    [FEAT] 서은표 납치엔딩으로 이동 추가

--- DateWithKing/Assets/Scripts/Screen/Semester/KarmaDirection.cs
+++ DateWithKing/Assets/Scripts/Screen/Semester/KarmaDirection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KarmaDirection : MonoBehaviour
 {
@@ -9,5 +10,19 @@ public class KarmaDirection : MonoBehaviour
     {
         GameManager.Instance.data.stats["karma"].StatChanged -= SoundManager.Instance.AddHorrorEffect;
         GameManager.Instance.data.stats["karma"].StatChanged += SoundManager.Instance.AddHorrorEffect;
+        SemesterSceneData.Instance.clock.TimeChanged -= Kidnap;
+        SemesterSceneData.Instance.clock.TimeChanged += Kidnap;
+    }
+
+    void Kidnap()
+    {
+        if (SemesterSceneData.Instance.clock.GetCurrentWeekCycle() != WeekCycle.Night) return;
+        
+        if (GameManager.Instance.data.stats["lvPyo"].value >= 100 &&
+            GameManager.Instance.data.stats["karma"].value >= 7)
+        {
+            Debug.Log("서은표 납치엔딩을 시작합니다. ");
+            SceneManager.LoadScene("Ending");
+        }
     }
 }
