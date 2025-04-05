6d8275a458cd5edbfc3f52b7708cb65ed848fea7
Ssomewhat <isc10120@gmail.com>
2025-04-05 23:11:49 +0900

    [FEAT] 농구 미니게임 구현

---
+++ DateWithKing/Assets/Scripts/Minigame/Basketball/BasketballManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Yarn.Unity;
+
+public class BasketballManager : SceneSingleton<BasketballManager>
+{
+    [SerializeField] private Ball[] balls;
+    public event Action StartGame;
+    public event Action EndGame;
+    private int score = 0;
+    const int CLEAR_SCORE = 3;
+    const int TICKET = 5;
+    const int TIME_LIMIT = 20;
+    private bool gameOverSemaphore = true;
+
+    void Start()
+    {
+        BackgroundController.Instance.ChangeImage(Background.미니게임);
+        YarnManager.Instance.RunDialogue("종강총회_농구게임_시작");
+        SoundManager.Instance.PlaySFX("미니게임_시작");
+    }
+
+    [YarnCommand("PlayBasketball")]
+    public void PlayBasketball(){
+        NumTimer.Instance.StartTimer(TIME_LIMIT, GameOver);
+        StartGame?.Invoke();
+    }
+
+    public Ball GetBall(){
+        foreach(Ball ball in balls){
+            if(ball.isReady) return ball;
+        }
+        return null;
+    }
+
+    public void ScoreUp(){
+        score++;
+        ScoreText.Instance.SetScore(score);
+        if(score >= CLEAR_SCORE){
+            GameOver();
+        }
+    }
+
+    private void GameOver(){
+        if(gameOverSemaphore) gameOverSemaphore = false;
+        else return;
+
+        EndGame?.Invoke();
+        NumTimer.Instance.HideTimer();
+
+        if(score >= CLEAR_SCORE){
+            SoundManager.Instance.PlaySFX("미니게임_성공");
+            YarnManager.Instance.RunDialogue("종강총회_농구게임_성공");
+            GameManager.Instance.ticket += TICKET;
+        }
+        else{
+            SoundManager.Instance.PlaySFX("미니게임_게임오버");
+            YarnManager.Instance.RunDialogue("종강총회_농구게임_실패");
+        }
+    }
+
+}
