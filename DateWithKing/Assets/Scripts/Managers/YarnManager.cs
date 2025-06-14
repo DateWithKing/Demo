0d51d04a96acfa6b85c3f38aa798041a7e59a7a0
Ssomewhat <isc10120@gmail.com>
2025-06-14 21:43:05 +0900

    [FEAT] 밤에는 _ver2로 show하기
    편의점_서은표 가 들어간 노드 제외

--- DateWithKing/Assets/Scripts/Managers/YarnManager.cs
+++ DateWithKing/Assets/Scripts/Managers/YarnManager.cs
@@ -53,7 +53,7 @@ public class YarnManager : SceneSingleton<YarnManager>
     private event Action prevChoice;
     private string prevChoiceDialogue;
     private string prevChoiceDialogueCharacter;
-    private string prevChoiceDialogueName;
+    private string prevChoiceDialogueName = "";
     GameObject posButton;
     GameObject negButton;
 
@@ -157,7 +157,15 @@ public class YarnManager : SceneSingleton<YarnManager>
     /// </summary>
     /// <param name="spriteName">Resorces/Sprites 폴더에 있는 스프라이트여야 함</param>
     void ShowCharactor(string spriteName){
+        if(isNight() && !spriteName.Contains("_ver2") 
+        && !prevChoiceDialogueName.Contains("편의점_서은표")){  // 주4 밤 편의점 서은표 예외처리
+            spriteName += "_ver2";
+        }
         CharacterImage.sprite = Resources.Load<Sprite>("Sprites/Charactor/"+spriteName);
+        if(CharacterImage.sprite == null){
+            Debug.LogError("이미지가 존재하지 않음: "+spriteName);
+            return;
+        }
         CharacterImage.SetNativeSize();
         CharacterImage.gameObject.SetActive(true);
     }
