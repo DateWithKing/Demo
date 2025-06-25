27353a5f01122f9833d887cea7df742345755fe6
강승연 <syeon.dev@gmail.com>
2025-06-25 15:38:19 +0900

    [UPDATE] 분기 넘어갈 때마다 1초 클릭 블락

--- DateWithKing/Assets/Scripts/Screen/Semester/DayScreen/DayNightFade.cs
+++ DateWithKing/Assets/Scripts/Screen/Semester/DayScreen/DayNightFade.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DayNightFade : MonoBehaviour
 {
     public CanvasGroup blackScreen; // 검정색 화면을 위한 CanvasGroup
+    public Image blackImage;
     public GameObject targetObject;  // 비활성화할 게임 오브젝트
     public UnityEvent AfterFadeOut;
 
     void Start()
     {
-        blackScreen.alpha = 0; 
+        blackScreen.alpha = 0;
+        blackImage = blackScreen.GetComponent<Image>();
     }
 
     // 검정색 화면 페이드 인 메서드
@@ -19,6 +22,7 @@ public class DayNightFade : MonoBehaviour
         SoundManager.Instance.PlaySFX("버튼_낮밤전환");
         blackScreen.alpha = 0; 
         blackScreen.gameObject.SetActive(true); // 검정색 화면 활성화
+        blackImage.raycastTarget = true;
         blackScreen.DOFade(1, 1).OnComplete(DisableTargetObject); // 1초 동안 페이드 인 후 게임 오브젝트 비활성화
     }
 
@@ -32,6 +36,12 @@ public class DayNightFade : MonoBehaviour
     // 검정색 화면 페이드 아웃 메서드
     private void FadeOutBlackScreen()
     {
-        blackScreen.DOFade(0, 1).OnComplete(AfterFadeOut.Invoke); // 1초 동안 페이드 아웃
+        blackScreen.DOFade(0, 1).OnComplete(RidBlockAfterFading); // 1초 동안 페이드 아웃
+    }
+
+    private void RidBlockAfterFading()
+    {
+        blackImage.raycastTarget = false;
+        AfterFadeOut.Invoke();
     }
 }
