25ea0bb57e81c6ca0131d383cf5fec48dd62467d
Ssomewhat <isc10120@gmail.com>
2025-08-21 18:07:45 +0900

    [FEAT] 스크린샷 기능 구현
    S키 누를시 바탕화면 JaamNaMi에 저장됨

---
+++ DateWithKing/Assets/Scripts/Util/ScreenCapturer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class ScreenCapturer : Singleton<ScreenCapturer>
+{
+    string screenshotPath;
+    void Start()
+    {
+        screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "JaamNaMi");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (!Directory.Exists(screenshotPath))
+            {
+                Directory.CreateDirectory(screenshotPath);
+            }
+            CaptureScreen();
+        }
+    }
+    public void CaptureScreen()
+    {
+        string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+        string fileName = timestamp + ".png";
+        string fullPath = Path.Combine(screenshotPath, fileName);
+        
+        ScreenCapture.CaptureScreenshot(fullPath);
+        Debug.Log("스크린샷 저장: " + fullPath);
+    }
+}
